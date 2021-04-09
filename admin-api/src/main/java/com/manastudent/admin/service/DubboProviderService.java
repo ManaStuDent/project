ed9b52627533b7d9a828a00ad8a4a65121217ddd
LiuChenglong <long7@vip.qq.com>
2021-04-09 21:42:00 +0800

    temp commit

--- admin-api/src/main/java/com/manastudent/admin/service/DubboProviderService.java
+++ admin-api/src/main/java/com/manastudent/admin/service/DubboProviderService.java
@@ -2,19 +2,24 @@ package com.manastudent.admin.service;
 
 import com.manastudent.api.DemoService;
 import org.apache.dubbo.config.annotation.Service;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.concurrent.CompletableFuture;
 
 
 @Service
 public class DubboProviderService implements DemoService {
+
+    @Value("${server.port}")
+    private String port;
+
     @Override
     public String sayHello(String msg) {
-        return "hello " + msg;
+        return "hello " + msg + " " + port;
     }
 
     @Override
     public CompletableFuture<String> sayHelloAsync(String msg) {
-        return CompletableFuture.supplyAsync(() -> "hello " + msg);
+        return CompletableFuture.supplyAsync(() -> "hello " + msg + " " + port);
     }
 }
